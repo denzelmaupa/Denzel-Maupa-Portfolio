313e749b72a2f16c05ed6812a9eb3a563c829205
Denzel Maupa <denzelmaupa@gmail.com>
2023-03-17 10:08:42 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var typed = new Typed(".autotype", {
+    strings: ["Creative", "Minimalistic", "Relentless"],
+    typeSpeed: 140,
+    backSpeed: 80,
+    loop: true
+})
+
+const toggleNav = () => {
+    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : true;
+}
+
+document.getElementById("cards").onmousemove = e => {
+    for (const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    }
+};

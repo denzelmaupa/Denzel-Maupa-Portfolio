30f3ceeb9eeca997cbec9956fdc80d31531033bc
Denzel Maupa <denzelmaupa@gmail.com>
2023-03-23 09:25:44 +0200

    Fourth Commit Updated Phone Padding and Margin. Created New Pages and Styles and seperated nav

---
+++ js/about.js
@@ -0,0 +1,10 @@
+const toggleNav = () => {
+    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : true;
+}
+
+var typed = new Typed(".autotype", {
+    strings: ["Designer", "Developer", "Digital Marketer"],
+    typeSpeed: 140,
+    backSpeed: 80,
+    loop: true
+})

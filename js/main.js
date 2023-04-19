854885795b11f5105ad53df561c0d0ad17c8e812
Denzel Maupa <denzelmaupa@gmail.com>
2023-04-19 22:39:33 +0200

    Changed and fixed my services links, and button links (most of em)
    \f

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-var typed = new Typed(".autotype", {
-    strings: ["Creative", "Minimalistic", "Relentless"],
-    typeSpeed: 140,
-    backSpeed: 80,
-    loop: true
-})
-
-const toggleNav = () => {
-    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : true;
-}
-
 document.getElementById("cards").onmousemove = e => {
     for (const card of document.getElementsByClassName("card")) {
         const rect = card.getBoundingClientRect(),
@@ -18,3 +7,31 @@ document.getElementById("cards").onmousemove = e => {
         card.style.setProperty("--mouse-y", `${y}px`);
     }
 };
+
+
+const toggleNav = () => {
+    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : true;
+};
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.yourisp.com",
+        Username: "username",
+        Password: "password",
+        To: "denzelmaupa@gmail.com",
+        From: document.getElementById("email").value,
+        Subject: "New Contact Form Enquiry",
+        Body: "Name: " + document.getElementById("name").value 
+        + "<br> Email: " + document.getElementById("email").value 
+        + "<br> Message: " + document.getElementById("message").value
+    }).then(
+        message => alert("Message Sent Successfully")
+    );
+};
+
+var typed = new Typed(".autotype", {
+    strings: ["Creative", "Minimalistic", "Relentless"],
+    typeSpeed: 140,
+    backSpeed: 80,
+    loop: true
+});

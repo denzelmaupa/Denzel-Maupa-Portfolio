d819e727d1c80d31b7c9a5c5faab8a14370e8458
Denzel Maupa <denzelmaupa@gmail.com>
2023-06-12 11:29:57 +0200

    Fully Edited the Biography Page

--- js/about.js
+++ js/about.js
@@ -2,9 +2,11 @@
 //     document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : true;
 // }
 
+
 var typed = new Typed(".autotype", {
     strings: ["Designer", "Developer", "Digital Marketer"],
     typeSpeed: 140,
     backSpeed: 80,
     loop: true
-})
+});
+
--- js/main.js
+++ js/main.js
@@ -9,3 +9,12 @@ $.get("nav.html", function (data) {
 $(function () {
     $("#footer").load("footer.html");
 });
+
+window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+
+    denzel.style.width = value * .1 + "%";
+    denzelphone.style.width = value * .2 + "%";
+
+});
+
